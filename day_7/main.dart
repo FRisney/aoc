178bc3691115b78a902a9ae9e301e9c0e3395b9d
Felipe Risney <41243324+FRisney@users.noreply.github.com>
2023-10-03 17:57:13 -0300

    feat: Day 7 part 2

--- day_7/main.dart
+++ day_7/main.dart
@@ -3,15 +3,27 @@ import 'dart:io';
 import 'src/console.dart';
 
 void main() {
+  const DISCSPACE = 70000000;
+  const UPDATESIZE=30000000;
   final log = File('day_7/input').readAsStringSync();
 
   final console = Console();
   console.reviveFromLog(log);
 
-  final list = console.root.filterDirectories((dir) => dir.size < 100000);
+  var list = console.root.filterDirectories((dir) => dir.size < 100000);
 
   final sumDirSize = list.fold<int>(0, (size, dir) => size + dir.size);
 
   print('Find all of the directories with a total size of at most 100000.\nWhat is the sum of the total sizes of those directories?');
   print("\t$sumDirSize");
+
+  final availableSpace = DISCSPACE - console.root.size;
+  final neededSpace = UPDATESIZE - availableSpace;
+
+  list = console.root.filterDirectories((dir) => dir.size > neededSpace);
+  list.sort((current, comparable) => current.size.compareTo(comparable.size));
+  final smallestDirSize = list.first.size;
+
+  print('Find the smallest directory that, if deleted, would free up enough space on the filesystem to run the update. What is the total size of that directory?');
+  print('\t$smallestDirSize');
 }
